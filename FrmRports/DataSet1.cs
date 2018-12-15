84c1c3317898d4b63dcf2e82e76e6c828aa07773
AMEMohammed <ame_mohammed@hotmail.com>
2018-12-15 19:39:57 +0300

    FrmRpot

---
+++ FrmRports/DataSet1.cs
@@ -0,0 +1,23 @@
+﻿namespace FrmRports
+{
+
+
+    partial class DataSet1
+    {
+        partial class SependDataTable
+        {
+        }
+
+        partial class SendTransferDataTable
+        {
+        }
+
+        partial class exit1DataTable
+        {
+        }
+
+        partial class AccountRevealDataTable
+        {
+        }
+    }
+}
---
+++ FrmRports/frmReprt.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+namespace FrmRports
+{
+    public partial class frmReprt : Form
+    {
+        int Tag1 = -1;
+        int Id = -1;
+        int User = 0;
+        DataTable dt1 = new DataTable();
+        DataTable dt2 = new DataTable();
+        public frmReprt()
+        {
+            InitializeComponent();
+        }
+        public frmReprt(DataTable dt ,int Tag)
+        {
+            InitializeComponent();
+            dt1= dt;
+            Tag1 = Tag;
+        }
+        
+        private void frmReprt_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                switch(Tag1)
+                {
+                    case 1:    PrintSendTrnsfer();
+                        break;
+                    case 2:    PrintReicveTrnsfer();
+                        break;
+                    case 3:     PrintDeposet();
+                        break;
+                    case 4:     PrintSpend();
+                        break;
+                }
+                
+
+            }
+            catch(Exception ex) { MessageBox.Show(ex.Message); }
+                
+
+        }
+        public void PrintSendTrnsfer()
+        {
+          
+            CrystalSendTransferr rp = new CrystalSendTransferr();
+            rp.SetDataSource(dt1);
+            crystalReportViewer1.ReportSource = rp;
+            crystalReportViewer1.Refresh();
+
+        }
+        public void PrintReicveTrnsfer()
+        {
+            CrystalReicve rp = new CrystalReicve();
+            rp.SetDataSource(dt1);
+            crystalReportViewer1.ReportSource = rp;
+            crystalReportViewer1.Refresh();
+
+        }
+        public void PrintDeposet()
+        {
+            CrystalDeposet rp = new CrystalDeposet();
+            rp.SetDataSource(dt1);
+            crystalReportViewer1.ReportSource = rp;
+            crystalReportViewer1.Refresh();
+
+        }
+        public void PrintSpend()
+        {
+            CrystalSpend rp = new CrystalSpend();
+            rp.SetDataSource(dt1);
+            crystalReportViewer1.ReportSource = rp;
+            crystalReportViewer1.Refresh();
+
+        }
+
+
+    }
+}
